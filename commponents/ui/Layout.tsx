f1adb522750656c0e2689734a91169d1854b09bd
rima2355 <rimazulita2@gmail.com>
2025-12-23 10:14:39 +0700

    Update dependencies and refactor page components. Added axios, js-cookie, and related types to package.json and package-lock.json. Refactored Home and ProductCategoryPage components to use a new Layout component for improved structure.

---
+++ commponents/ui/Layout.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import React from 'react'
+import Navbar from './Navbar';
+
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
+    <main className="flex min-h-screen w-full flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
+     
+      <Navbar/>
+      {children}
+    </main>
+  </div>
+  )
+}
---
+++ commponents/ui/Navbar.tsx
@@ -0,0 +1,47 @@
+import Link from 'next/link'
+import React from 'react'
+
+
+export default function Navbar() {
+  return (
+    <nav className="w-full relative bg-gray-800/50 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            {/* <!-- Mobile menu button--> */}
+            <button type="button" 
+                className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
+              <span className="absolute -inset-0.5"></span>
+              <span className="sr-only">Open main menu</span>
+            </button>
+          </div>
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex shrink-0 items-center">
+              <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company"className="h-8 w-auto" />
+            </div>
+            <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4">
+                {/* <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" --> */}
+                <Link href="/" aria-current="page"className="rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white">
+                Home
+                </Link>
+                <Link href="/product-category"className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">
+                Product Category
+                </Link>
+                <Link href="/product"className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">
+                Product
+                </Link>
+                <Link href="/Product-variant"className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">
+                Product variant
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+
+    </nav>
+    
+  )
+}
